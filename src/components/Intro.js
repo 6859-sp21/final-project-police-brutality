1bb1fd4b35c5597c4009718c7b0aff86cf21aff9
Tanya Yang <tanyang@mit.edu>
2021-05-01 20:11:44 -0400

    added react version

---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div>
+      <h3>police brutality in the united states</h3>
+    </div>
+  );
+};
+
+export default Intro;
--- src/index.js
+++ src/index.js
@@ -1,43 +1,11 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
-mapboxgl.workerClass = MapboxWorker;
-mapboxgl.accessToken = 'pk.eyJ1IjoidGFueWFuZyIsImEiOiJjanl0ZjJzaHUwM2dwM2xtaDJtNzlleWs4In0.3yIc4Cb3MrfjM8Kr1EoyhA';
+import './index.css';
+import App from './App';
 
-const Map = () => {
-  const mapContainer = useRef();
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
-
-    map.on('move', () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
-    });
-
-    return () => map.remove();
-  }, []);
-
-  return (
-    <div>
-      hello
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div className="map-container" ref={mapContainer} />
-    </div>
-  );
-};
-
-ReactDOM.render(<Map />, document.getElementById('app'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
