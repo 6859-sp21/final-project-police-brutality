9bd7f893d4547a762fcf33419529656171ee9fc1
Tanya Yang <tanyang@mit.edu>
2021-05-02 22:10:57 -0400

    attempted circle chart

--- d3_vis.js
+++ d3_vis.js
@@ -1,7 +1,49 @@
 d3.csv("https://raw.githubusercontent.com/6859-sp21/final-project-police-brutality/main/data/fatal-police-shootings-data.csv").then((data) => {
     fpsData = data;
     d3_test();
+    d3_racism();
 });
+
+function d3_racism() {
+  var width = 960, 
+    height = 500, 
+    radius = Math.min(width, height) / 2;
+
+  var svg = d3.select("#d3-racism")
+    .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  
+  var data = {"White": 3972, "Black": 2288, "Hispanic": 1594}
+  
+  var color = d3.scaleOrdinal()
+    .domain(data)
+    .range(d3.schemeSet2);
+
+  var pie = d3.pie()
+    .value(function(d) {return d.value})
+  var data_ready = pie(d3.entries(data))
+
+  // shape helper to build arcs:
+  var arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(radius)
+
+  // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+  svg
+  .selectAll('mySlices')
+  .data(data_ready)
+  .enter()
+  .append('path')
+  .attr('d', arcGenerator)
+  .attr('fill', function(d){ return(color(d.data.key)) })
+  .attr("stroke", "black")
+  .style("stroke-width", "2px")
+  .style("opacity", 0.7)
+
+  }
 
 function d3_test() {
     testData = fpsData.slice(6200).filter(d => d.age !== "" && d.name !== "");
