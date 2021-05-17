d18a880e1dda519c784583b6a0d8249db5a4be3e
Allanaz <gzych.allan@gmail.com>
2021-05-17 02:48:05 -0400

    process data into shape for stack, and extract cumulative numbers for visualization

--- excelProcessor.py
+++ excelProcessor.py
@@ -3,6 +3,20 @@ import pandas as pd
 df_sheet_all = pd.read_excel('data/DISP_AllStatesAndTerritories.xlsx', sheet_name=None)
 
 result = pd.concat(df_sheet_all, ignore_index=True, sort=False)
+CA = df_sheet_all['California']
+print(CA.dtypes)
+categoryA = CA.loc[(pd.DatetimeIndex(CA['Ship Date']).year <= 2017) & (CA['DEMIL Code'] == 'A')]
+print(categoryA['Acquisition Value'].sum())
 
+# result.to_csv("data/all1033.csv", index=False)
+columns = ['year', 'A', 'B', 'C', 'D', 'E', 'F', 'Q']
+data = []
 
-result.to_csv("data/all1033.csv", index=False)
+for year in range(1991, 2018):
+    row = [year, ]
+    for category in columns[1:]:
+        row.append(result.loc[(pd.DatetimeIndex(result['Ship Date']).year <= year) & (result['DEMIL Code'] == category)]['Acquisition Value'].sum())
+    data.append(row)
+
+timeSeriesCategories = pd.DataFrame(data, columns=columns)
+timeSeriesCategories.to_csv("data/all1033Categories.csv", index=False)
---
+++ militarization.js
@@ -0,0 +1,18 @@
+
+// set the dimensions and margins of the graph
+var margin = {top: 60, right: 230, bottom: 50, left: 50},
+    width = 660 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("https://raw.githubusercontent.com/6859-sp21/final-project-police-brutality/main/data/all1033.csv").then((data) => {
+
+});
